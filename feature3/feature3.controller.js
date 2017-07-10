b6ec62cc306c8fad4a7e6f07199b01f55935609c
Savan-Karathiya <savan.karathiya@rapidops.com>
2017-07-10 18:29:26 +0530

    Added more stuff in controller of feature3

--- feature3/feature3.controller.js
+++ feature3/feature3.controller.js
@@ -1,2 +1,6 @@
 console.log('This is extra line for conflict');
 console.log('This is controller file for feature3');
+console.log('This is extra stuff');
+console.log('This is extra line for conflict');
+console.log('This is controller file for feature3');
+console.log('This is extra stuff');
