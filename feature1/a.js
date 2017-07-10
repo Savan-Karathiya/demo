7c43c415fbd0eba706ca63b44631536e684a7dc9
Savan-Karathiya <savan.karathiya@rapidops.com>
2017-07-10 17:23:11 +0530

    Added extra line in feature index.js file
    Added 2nd line in a.js file
    
    Added h2 tag in html file

--- feature1/a.js
+++ feature1/a.js
@@ -1,1 +1,2 @@
-console.log('This is a.js file.');
+console.log('This is 1st line of a.js file.');
+console.log('This is 2nd line of a.js file.');
--- feature1/index.js
+++ feature1/index.js
@@ -1,1 +1,2 @@
 console.log('This is index.js file of feature1.');
+console.log('This is index.js file of feature1.');
