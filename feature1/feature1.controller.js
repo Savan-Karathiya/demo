c5ba6a137b19af97a3197c49653339a45f22b32f
Savan-Karathiya <savan.karathiya@rapidops.com>
2017-07-10 19:47:12 +0530

    Added some stuff in controller of feature1

--- feature1/feature1.controller.js
+++ feature1/feature1.controller.js
@@ -1,2 +1,4 @@
+console.log('This is feature3 controller file.');
 console.log('This is feature1 controller file.');
+console.log('This is feature2 controller file.');
 console.log('This is extra line in controller file.');
